a2655c543e2129a9f93b81ef07e91d972e42ab18
Perihan Süleymanoğlu <47482913+perihansuleymanoglu@users.noreply.github.com>
2021-02-03 14:07:44 +0300

    Add project files.

---
+++ GameProject/Campaign.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject
+{
+    class Campaign
+    {
+        public int campaignId { get; set; }
+        public string campaignName { get; set; }
+    }
+}
---
+++ GameProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GameProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GamerManager gamerManager = new GamerManager(new UserValidationManager());
+            gamerManager.Add(new Gamer {Id =1 , BirthYear =1997, FirstName="Perihan", LastName="Süleymanoğlu", IdentityNumber= 987463 });
+
+
+            CampaignManager campaignManager = new CampaignManager();
+            campaignManager.Add(new Campaign {campaignId=1, campaignName="Tüm oyunlarda %30 indirim" });
+        }
+    }   
+}
